ce7251bee2e632364986627408daad37320d34b3
BorisPavlov <bpavlov@mail.ru>
2020-09-23 16:23:23 +0900

    Commit

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution() {}
+    protected Solution(String s) {}
+    Solution(String s, String d) {}
+    private Solution(double d) {}
+
+    public static void main(String[] args) {
+
+    }
+}
+

e26fba5393f5d34881735687583c38fd2f1b9a6f
BorisPavlov <bpavlov@mail.ru>
2020-09-29 14:58:31 +0900

    Commit

--- 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task16.task1629;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.Exception;
 
 /* 
 Только по-очереди!
@@ -16,10 +17,33 @@ public class Solution {
         Read3Strings t2 = new Read3Strings();
 
         //add your code here - добавьте код тут
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
 
         t1.printResult();
         t2.printResult();
     }
 
     //add your code here - добавьте код тут
+    public static class Read3Strings extends Thread {
+
+        public String str = "";
+
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                for (int i = 0; i < 3; i++) {
+                    str += reader.readLine() + " ";
+                }
+                reader.close();
+            } catch (IOException e) {
+            }
+        }
+
+        public void printResult() {
+            System.out.println(this.str);
+        }
+    }
 }

3fb118ece72aac58f1855c0a1a43299e7965e5bd
BorisPavlov <bpavlov@mail.ru>
2020-10-01 16:03:18 +0900

    Commit

--- 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -2,12 +2,27 @@ package com.javarush.task.task16.task1631;
 
 import com.javarush.task.task16.task1631.common.ImageReader;
 import com.javarush.task.task16.task1631.common.ImageTypes;
+import com.javarush.task.task16.task1631.common.JpgReader;
+import com.javarush.task.task16.task1631.common.BmpReader;
+import com.javarush.task.task16.task1631.common.PngReader;
 
 public class ImageReaderFactory {
-    public String getImageReader(String ImageTypes) {
+    public static ImageReader getImageReader(ImageTypes type) {
+        ImageReader result = null;
         try {
-
+            if(type == null) {throw new IllegalArgumentException("Неизвестный тип картинки");}
+            switch ( type ) {
+                case JPG:
+                    return new JpgReader();
+                case BMP:
+                    return new BmpReader();
+                case PNG:
+                    return new PngReader();
+            }
         }
-        catch
+        catch (Exception e) {
+            throw new IllegalArgumentException("Неизвестный тип картинки");
+        }
+        return result;
     }
 }

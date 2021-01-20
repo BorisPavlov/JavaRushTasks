03fa93143415c3bd00c2648adb4513f074354c71
BorisPavlov <bpavlov@mail.ru>
2021-01-20 12:31:58 +0900

    2021-01-21

--- 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
@@ -11,7 +11,7 @@ Externalizable для апартаментов
 
 public class Solution {
 
-    public static class Apartment {
+    public static class Apartment implements Externalizable {
 
         private String address;
         private int year;
@@ -31,6 +31,17 @@ public class Solution {
         /**
          * Prints out the fields used for testing!
          */
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(address);
+            out.writeInt(year);
+        }
+
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            address = (String) in.readObject();
+            year = in.readInt();
+        }
+
+
         public String toString() {
             return ("Address: " + address + "\n" + "Year: " + year);
         }
--- 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
@@ -8,14 +8,14 @@ import java.util.Date;
 Serializable Solution
 */
 
-public class Solution {
+public class Solution implements Serializable {
     public static void main(String[] args) {
         System.out.println(new Solution(4));
     }
 
-    private final String pattern = "dd MMMM yyyy, EEEE";
-    private Date currentDate;
-    private int temperature;
+    private final transient String pattern = "dd MMMM yyyy, EEEE";
+    private transient Date currentDate;
+    private transient int temperature;
     String string;
 
     public Solution(int temperature) {
